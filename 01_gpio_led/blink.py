6f8dbb0292b2949b63a015c61ca8c1efe0d14ee4
YOO, SEONG-EUN <110365816+profsyoo@users.noreply.github.com>
2026-04-09 12:27:59 +0900

    Create blink.py

---
+++ 01_gpio_led/blink.py
@@ -0,0 +1,25 @@
+import time
+import lgpio
+
+# Configuration
+LED = 17
+chip = lgpio.gpiochip_open(0)
+
+# Claim the GPIO pin for output
+lgpio.gpio_claim_output(chip, LED)
+
+try:
+    while True:
+        # Turn LED ON
+        lgpio.gpio_write(chip, LED, 1)
+        time.sleep(0.5)
+        
+        # Turn LED OFF
+        lgpio.gpio_write(chip, LED, 0)
+        time.sleep(0.5)
+
+except KeyboardInterrupt:
+    # Cleanup on exit
+    print("\nProgram stopped by user")
+    lgpio.gpio_write(chip, LED, 0)
+    lgpio.gpiochip_close(chip)
