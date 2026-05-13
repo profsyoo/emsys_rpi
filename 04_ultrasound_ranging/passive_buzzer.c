8525ea011169c20b2355ba0f562d0b83083e0fee
Sung (aka Seong-eun) Yoo <110365816+profsyoo@users.noreply.github.com>
2026-05-13 18:49:42 +0900

    Update passive_buzzer.c

--- 04_ultrasound_ranging/passive_buzzer.c
+++ 04_ultrasound_ranging/passive_buzzer.c
@@ -20,27 +20,27 @@ int main(void)
     printf("Raspberry Pi Passive Buzzer test program (lgpio)\n");
     printf("Press Ctrl+C to exit\n");
 
-    h = gpiochip_open(0);                     // Open gpiochip0
-    gpio_claim_output(h, BUZZER, 0);           // Set BUZZER pin as output
+    h = lgGpiochipOpen(0);                     // Open gpiochip0
+    lgGpioClaimOutput(h, 0, BUZZER, 0);           // Set BUZZER pin as output
 
     while (1)
     {
         for (int i = 0; i < 80; i++)           // Output frequency sound
         {
-            gpio_write(h, BUZZER, 1);          // Sound ON
+            lgGpioWrite(h, BUZZER, 1);          // Sound ON
             usleep(1000);                      // 1 ms delay
-            gpio_write(h, BUZZER, 0);          // Sound OFF
+            lgGpioWrite(h, BUZZER, 0);          // Sound OFF
             usleep(1000);                      // 1 ms delay
         }
 
         for (int j = 0; j < 100; j++)          // Output another frequency
         {
-            gpio_write(h, BUZZER, 1);          // Sound ON
-            gpio_write(h, BUZZER, 0);          // Sound OFF
+            lgGpioWrite(h, BUZZER, 1);          // Sound ON
+            lgGpioWrite(h, BUZZER, 0);          // Sound OFF
             usleep(2000);                      // 2 ms delay
         }
     }
 
-    gpiochip_close(h);                         // Release GPIO resources
+    lgGpiochipClose(h);                         // Release GPIO resources
     return 0;
 }
