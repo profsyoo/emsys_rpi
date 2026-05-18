ab6b709325cc491eefd4517a7a94b38995236bf6
Sung (aka Seong-eun) Yoo <110365816+profsyoo@users.noreply.github.com>
2026-05-18 14:48:45 +0900

    Update and rename 5-4.py to 5-4CaptureNSave.py

--- 05_usbwebcam/5-4CaptureNSave.py
+++ 05_usbwebcam/5-4CaptureNSave.py
@@ -1,9 +1,10 @@
+#may take some time
 import cv2
 camera = cv2.VideoCapture(0, cv2.CAP_V4L)
 ret, image = camera.read( )
 if ret == True:
-	# OpenCV 함수로 이미지를 파일에 저장
+	# Save the captured image via OpenCV function, imwrite
 	cv2.imwrite('image_5_4.jpg', image)
 else:
-	print('카메라로부터 프레임의 캡처할 수 없습니다.')
+	print("Can't capture any frame via. the camera")
 camera.release( )
