013956a78df1f3a8bfd9329326d1ba9a2ba7723e
Sung (aka Seong-eun) Yoo <110365816+profsyoo@users.noreply.github.com>
2026-05-20 15:21:18 +0900

    Update 5-4CaptureNSave.py

--- 05_usbwebcam/5-4CaptureNSave.py
+++ 05_usbwebcam/5-4CaptureNSave.py
@@ -2,12 +2,17 @@
 import cv2
 
 try:
-camera = cv2.VideoCapture(0, cv2.CAP_V4L)
-ret, image = camera.read( )
-if ret == True:
-	# Save the captured image via OpenCV function, imwrite
-	cv2.imwrite('image_5_4.jpg', image)
-else:
-	print("Can't capture any frame via. the camera")
+  camera = cv2.VideoCapture(0, cv2.CAP_V4L)
+  ret, image = camera.read( )
+  if ret == True:
+	  # Save the captured image via OpenCV function, imwrite
+	  cv2.imwrite('image_5_4.jpg', image)
+  else:
+  	print("Can't capture any frame via. the camera")
 
-camera.release( )
+except KeyboardInterrupt:
+  pass
+	
+finally:	
+  camera.release( )
+
