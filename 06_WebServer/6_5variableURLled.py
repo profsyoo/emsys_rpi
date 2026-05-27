84b54e25cabf4f9190ef692d3017a67a76c872ff
Sung (aka Seong-eun) Yoo <110365816+profsyoo@users.noreply.github.com>
2026-05-27 15:58:36 +0900

    Update 6_5variableURLled.py

--- 06_WebServer/6_5variableURLled.py
+++ 06_WebServer/6_5variableURLled.py
@@ -32,7 +32,8 @@ def hello(name):
 
 if __name__ == '__main__':
 	print("Before")
-	app.run(host='0.0.0.0', port=8080, debug=True)
+#	app.run(host='0.0.0.0', port=8080, debug=True)
+	app.run(host='0.0.0.0', port=8080, debug=False)
 
 	print("Ending")
 
